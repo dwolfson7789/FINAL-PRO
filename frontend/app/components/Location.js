2694513cb1b2c3af7ed89a151541154fedd22ae0
dwolfson7789 <Drew@Drews-MacBook-Pro.local>
2016-05-02 12:50:03 -0400

    Master 1

--- frontend/app/components/Location.js
+++ frontend/app/components/Location.js
@@ -9,9 +9,25 @@ const Location = React.createClass({
     return(
       <form>
         <label>
-          <h3>Enter your location</h3>
-          <br />
-          <input type="text" id="location-input" onChange={this.submitLocation}></input>&nbsp;
+          // search by location //
+          <div className="dropDown">
+            <h3>By Location:</h3>
+              <select onChange={this.props.onChangeLocation}>
+                  <option placeholder='select location' value=''>select location</option>
+                  <option value='Chelsea'>Chelsea</option>
+                  <option value='Financial District'>Financial District</option>
+                  <option value='Flatiron'>Flatiron</option>
+                  <option value='Hells Kitchen'>Hells Kitchen</option>
+                  <option value='Meatpacking'>Meatpacking</option>
+                  <option value='Midtown East'>Midtown East</option>
+                  <option value='Soho'>Soho</option>
+                  <option value='Tribeca'>Tribeca</option>
+                  <option value='Upper East'>Upper East</option>
+                  <option value='Upper West'>Upper West</option>
+              </select>
+          // <h3>Enter your location</h3>
+          <br/>
+          <input type="text" class= "userInput" onChange={this.submitLocation}></input>&nbsp;
           <button type="button">Go</button>
         </label>
       </form>
